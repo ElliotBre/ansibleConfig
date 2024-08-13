f6f8b01adcaeaf4a88e8ed4c8229a0627d5e9494
ElliotBre <124515858+ElliotBre@users.noreply.github.com>
2024-08-13 12:28:16 +0100

    adding hosts file, setting up initial playbooks

---
+++ resources/get-initial-admin-password.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+until [ -f .jenkins/secrets/initialAdminPassword ]; do
+    sleep 1
+    echo "waiting for initial admin password"
+done
+until [[ -n "\$(cat  .jenkins/secrets/initialAdminPassword)" ]]; do
+    sleep 1
+    echo "waiting for initial admin password"
+done
+echo "initial admin password: \$(cat .jenkins/secrets/initialAdminPassword)"
